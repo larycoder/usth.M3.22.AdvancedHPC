37319671de35612cbca18ac50c67429c239c379f
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2021-11-08 09:03:42 -0500

    LH    : ex1 - quest2 - add vector by iterator

--- LW1/ex1/Question2.cu
+++ LW1/ex1/Question2.cu
@@ -2,8 +2,14 @@
 #include <include/chronoGPU.hpp>
 #include <thrust/device_vector.h>
 
-namespace {
-	/// function that adds two elements and return a new one	
+namespace func{
+	/// function that adds two elements and return a new one
+    class AddFunctor : public thrust::binary_function<int, int, int> {
+    public:
+        __host__ __device__ int operator()(const int &x, const int &y) {
+            return x + y;
+        }
+    };
 }
 
 
@@ -14,12 +20,21 @@ void Exercise::Question2(thrust::host_vector<int>&A) const
 	for(int i=3; i--; ) {
 		chrUp.start();
 		// TODO: Move Data to DEVICE
+        thrust::device_vector<int> dA(m_size);
+        thrust::constant_iterator<int> constIter(5);
+        thrust::counting_iterator<int> countIter(0);
 		chrUp.stop();
 		chrCalc.start();
 		// TODO: Do the MAP
+        thrust::transform(
+                constIter, constIter + m_size,
+                countIter,
+                dA.begin(),
+                func::AddFunctor());
 		chrCalc.stop();
 		chrDown.start();
 		// TODO: Move Data to HOST
+        A = dA;
 		chrDown.stop();
 	}        
 	std::cout <<"Exercise 2 done in: "<<chrUp.elapsedTime()+chrDown.elapsedTime()+chrCalc.elapsedTime()<<"ms"<<std::endl;
