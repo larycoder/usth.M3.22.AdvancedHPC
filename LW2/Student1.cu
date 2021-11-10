5820212b533399f5b7cdf11b4d9efee2c62a3bbf
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2021-11-10 06:51:13 -0500

    LH    : ex1 - matrices addition in device

--- LW2/Student1.cu
+++ LW2/Student1.cu
@@ -6,13 +6,18 @@
 #include <thrust/functional.h>
 
 bool D_Matrix::Exo1IsDone() {
-	return false;
+	return true;
 }
 
 // returns this times that ...
 D_Matrix D_Matrix::operator+(const D_Matrix& that) const
 {
-  // do "d_val + that.d_val" 
+  // do "d_val + that.d_val"
   D_Matrix result(m_n);
+  thrust::transform(
+          d_val, d_val + m_n*m_n,
+          that.d_val,
+          result.d_val,
+          thrust::plus<int>());
   return result;
 }
