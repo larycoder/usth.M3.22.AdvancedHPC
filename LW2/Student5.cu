0da04cbbe6da60c64e33a9219cba2811b673571f
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2021-11-11 07:37:31 -0500

    LH     : LW2 - ex5 - improve efficiency of ex4

--- LW2/Student5.cu
+++ LW2/Student5.cu
@@ -9,10 +9,68 @@
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/device_vector.h>
 
-bool D_Matrix::Exo5IsDone() {
-	return false;
+
+namespace {
+    class keyFunctor : public thrust::unary_function<int, int> {
+        const int m_n;
+
+    public:
+        keyFunctor(int m_n) : m_n(m_n) {}
+
+        keyFunctor() = delete;
+
+        __device__ int operator()(const int &in) {
+            return in / m_n;
+        }
+    };
+
+    class transformFunctor : public thrust::unary_function<int, int> {
+        const thrust::device_ptr<int> ptr1;
+        const thrust::device_ptr<int> ptr2;
+        const int line, m_n;
+
+    public:
+        transformFunctor(
+                const thrust::device_ptr<int> ptr1,
+                const thrust::device_ptr<int> ptr2,
+                const int line, const int m_n
+        ) : ptr1(ptr1), ptr2(ptr2), line(line), m_n(m_n) {}
+
+        transformFunctor() = delete;
+
+        __device__ int operator()(const int &in) {
+            return ptr1[in] * ptr2[in % m_n + line * m_n];
+        }
+    };
 }
+
+bool D_Matrix::Exo5IsDone() {
+    return true;
+}
+
 // returns this times that ...
-D_Matrix D_Matrix::product2(const D_Matrix& that) const {
-	return D_Matrix(m_n);
+D_Matrix D_Matrix::product2(const D_Matrix &that) const {
+    D_Matrix result(m_n);
+    thrust::device_vector<int> keyOut(m_n);
+
+    D_Matrix tpThat = that.transpose();
+    const int count = m_n * m_n;
+    auto keyIter = thrust::make_transform_iterator(
+            thrust::counting_iterator<int>(0),
+            keyFunctor(m_n));
+
+    for (int i = 0; i < m_n; i++) {
+        // map diffusion to matrix with * op
+        auto transformIter = thrust::make_transform_iterator(
+                thrust::counting_iterator<int>(0),
+                transformFunctor(d_val, tpThat.d_val, i, m_n));
+
+        // reduce to col
+        thrust::reduce_by_key(
+                thrust::device,
+                keyIter, keyIter + count,
+                transformIter,
+                keyOut.begin(), result.d_val + i * m_n);
+    }
+    return result.transpose();
 }
