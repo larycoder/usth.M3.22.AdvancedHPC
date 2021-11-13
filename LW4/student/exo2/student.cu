9b86d1c986885e9859a0a5635cb414b6a4acc86b
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2021-11-13 09:45:03 -0500

    LH    : LW4 - ex2 - compute histogram by GPU

--- LW4/student/exo2/student.cu
+++ LW4/student/exo2/student.cu
@@ -2,19 +2,29 @@
 #include <exo2/student.h>
 #include <OPP_cuda.cuh>
 
-namespace 
-{
+#define SIZE_HISTO 256.f
+
+namespace {
+    struct Functor {
+        __device__
+        unsigned operator () (const float &data ) const {
+            return (unsigned) (data * SIZE_HISTO);
+        }
+    };
 }
 
 bool StudentWorkImpl::isImplemented() const {
-	return true;
+    return true;
 }
 
 void StudentWorkImpl::run_Histogram(
-	OPP::CUDA::DeviceBuffer<float>& dev_value,
-	OPP::CUDA::DeviceBuffer<unsigned>& dev_histogram,
-	const unsigned width,
-	const unsigned height
+        OPP::CUDA::DeviceBuffer<float> &dev_value,
+        OPP::CUDA::DeviceBuffer<unsigned> &dev_histogram,
+        const unsigned width,
+        const unsigned height
 ) {
-	// TODO
+    // TODO
+    OPP::CUDA::computeHistogram<float, unsigned, Functor>(
+            dev_value, dev_histogram, Functor()
+    );
 }
